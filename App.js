f6eef6e8b2d470f64608ce4dbf79f5d270826420
Nnamdi Okeke <onnamdialex@gmail.com>
2023-07-19 17:02:08 +0100

    :art: Improve structure of the App. wip ...

--- App.js
+++ App.js
@@ -16,9 +16,12 @@ const theme = {
 const App = () => {
   return (
     <NavigationContainer theme={theme}>
-      <Stack.Navigator>
-        <Stack.Screen />
-        <Stack.Screen />
+      <Stack.Navigator
+        screenOptions={{ headerShown: false }}
+        initialRouteName="Home"
+      >
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Details" component={Details} />
       </Stack.Navigator>
     </NavigationContainer>
   );
