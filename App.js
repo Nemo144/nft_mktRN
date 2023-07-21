0fedff80fa19c32a7ab5940d9c2381e214c75388
Nnamdi Okeke <onnamdialex@gmail.com>
2023-07-21 18:31:48 +0100

    :zap: Add different fonts for use

--- App.js
+++ App.js
@@ -17,7 +17,17 @@ const theme = {
 
 const App = () => {
   //initiaizing the fonts to be used for dev
-  const [loaded] = useFonts({});
+  const [loaded] = useFonts({
+    InterBold: require("./assets/fonts/Inter-Bold.ttf"),
+    InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
+    InterMediumBold: require("./assets/fonts/Inter-Medium.ttf"),
+    InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
+    InterLight: require("./assets/fonts/Inter-Light.ttf"),
+  });
+
+  //if the fonts are not loaded, then return null
+  if (!loaded) return null;
+
   return (
     <NavigationContainer theme={theme}>
       <Stack.Navigator
