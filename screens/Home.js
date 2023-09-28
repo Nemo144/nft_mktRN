9e07500c54b6503804b13db695224f343c831f9c
Nnamdi Okeke <onnamdialex@gmail.com>
2023-09-28 14:29:01 +0100

    :zap: Performance maximization test

--- screens/Home.js
+++ screens/Home.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Text, SafeArea, FlatList, View } from "react-native";
-import { COLORS, NFTData } from "../constants";
+// import { COLORS, NFTData } from "../constants";
 
 const Home = () => {
   return <Text>Home</Text>;
