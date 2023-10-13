aa2f1d9de9fa949505a2f5784ecf6bfbe8feb8f2
Nnamdi Okeke <onnamdialex@gmail.com>
2023-10-13 22:24:05 +0100

    :bulb: Update comments

--- components/FocusedStatusBar.js
+++ components/FocusedStatusBar.js
@@ -2,7 +2,7 @@ import { StatusBar } from "react-native";
 import { useIsFocused } from "@react-navigation/core";
 
 const FocusedStatusBar = (props) => {
-  //calling the useIsFocused hook
+  //calling the useIsFocused hook for the status bar
   const isFocused = useIsFocused();
 
   return isFocused ? <StatusBar animated={true} {...props} /> : null;
