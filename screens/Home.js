db3be774fdc3d0931f1a76e0463cb26fce42812f
Nnamdi Okeke <onnamdialex@gmail.com>
2023-10-17 17:04:23 +0100

    :coffin: Remove dead code

--- screens/Home.js
+++ screens/Home.js
@@ -1,18 +1,3 @@
-// import { useState } from "react";
-// import { Text, FlatList, View, SafeAreaView } from "react-native";
-// import { COLORS, NFTData } from "../constants";
-// import { NFTCard, HomeHeader, FocusedStatusBar } from "../components";
-
-// const Home = () => {
-//   return (
-//     <SafeAreaView style={{ flex: 1 }}>
-//       <FocusedStatusBar background={COLORS.primary} />
-//     </SafeAreaView>
-//   );
-// };
-
-// export default Home;
-
 import { useState } from "react";
 import { Text, FlatList, View, SafeAreaView } from "react-native";
 import { COLORS, NFTData } from "../constants";
